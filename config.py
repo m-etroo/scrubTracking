7a52aae38b14d98b5e36a82814c2970682f5f611
mxtr0 <50273080+m-etroo@users.noreply.github.com>
2020-07-21 14:38:17 -0400

    Remove environment variables and input webhooks right into config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-from environs import Env
-env = Env()
-env.read_env()
-
 # Roblox game information
 GAME_UNIVERSE_ID = 300039023 # Universe id of game to track
 GAME_REQUEST_URL = f'https://games.roblox.com/v1/games?universeIds={GAME_UNIVERSE_ID}'
@@ -12,9 +8,9 @@ MEMBERS_REQUEST_URL = f'https://groups.roblox.com/v1/groups/{GROUP_ID}'
 
 # Webhook info
 DISCORD_WEBHOOKS = { # Dictionary of webhooks to send requests to
-    "visits": env("VISITS_WEBHOOK"), # Edit from .env file
-    "members": env("MEMBERS_WEBHOOK"), # Edit from .env file
-    "playing": env("PLAYING_WEBHOOK") # Edit from .env file
+    "visits": "", # Visits webhook
+    "members": "", # Members webhook
+    "playing": "" # Playing webhook
 }
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ" # Time format for tracker time on Discord
 VISITS_COUNTDOWN = 15000000 # Number of visits to countdown to
