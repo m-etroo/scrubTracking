95afaa29334d7566f9467ead231cf3dbfadcfbb6
m-etroo <50273080+m-etroo@users.noreply.github.com>
2020-07-21 18:56:43 -0400

    Add config fields for milestone mentions & code to make it work

--- config.py
+++ config.py
@@ -12,9 +12,43 @@ DISCORD_WEBHOOKS = { # Dictionary of webhooks to send requests to
     "members": "", # Members webhook
     "playing": "" # Playing webhook
 }
+MENTION_ROLE_ID = 999999999999999999 # Role id of role to mention for milestones
+VISITS_MILESTONES = open("visits_milestones.txt", "r").readlines() # Edit from visits_milestones.txt
+MEMBERS_MILESTONES = open("members_milestones.txt", "r").readlines() # Edit from members_milestones.txt
+
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ" # Time format for tracker time on Discord
 VISITS_COUNTDOWN = 1000000 # Milestone frequency (count down to every x number of visits)
 MEMBERS_COUNTDOWN = 10000 # Milestone frequency (count down to every x number of members)
 
 # Other
 INTERVAL = 60 # Number of seconds between tracking requests
+
+# ==================================================================================================
+# Do not modify below here
+# ==================================================================================================
+def remove_milestone(ms_type, milestone):
+    if ms_type == "visits":
+        VISITS_MILESTONES.remove(milestone) # Removes from current session list
+
+        # Removes for future runs of the script
+        milestones = open("visits_milestones", "r").readlines()
+        new_text = []
+        for line in milestones:
+            if milestone not in line:
+                new_text.append(milestone)
+        
+        milestones = open("visits_milestones", "w")
+        milestones.writelines(new_text)
+
+    elif ms_type == "members":
+        MEMBERS_MILESTONES.remove(milestone) # Removes from current session list
+        
+        # Removes for future runs of the script
+        milestones = open("members_milestones", "r").readlines()
+        new_text = []
+        for line in milestones:
+            if milestone not in line:
+                new_text.append(milestone)
+        
+        milestones = open("members_milestones", "w")
+        milestones.writelines(new_text)
