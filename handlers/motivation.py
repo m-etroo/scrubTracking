b22bb9f6406c6d10153d250dfe3c619a282dd600
m-etroo <50273080+m-etroo@users.noreply.github.com>
2020-07-23 14:43:27 -0400

    Add function to get motivation request url

---
+++ handlers/motivation.py
@@ -0,0 +1,8 @@
+import config
+
+def set_motivation():
+    if config.MOTIVATION_ENABLED = True:
+        # Get data from Roblox API
+        request = requests.get(api_urls.url(request_type))
+    else:
+        return ""
