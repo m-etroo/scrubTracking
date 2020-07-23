4fa79829dc9e0e4566f54638e26453b341d962f9
m-etroo <50273080+m-etroo@users.noreply.github.com>
2020-07-23 11:18:40 -0400

    combine api_urls into one function

--- handlers/api_urls.py
+++ handlers/api_urls.py
@@ -1,9 +1,9 @@
 import config
 
-def game_url():
-    uid = config.GAME_UNIVERSE_ID
-    return f'https://games.roblox.com/v1/games?universeIds={uid}'
-
-def group_url():
-    gid = config.GROUP_ID
-    return f'https://groups.roblox.com/v1/groups/{gid}'
+def url(request_type):
+    if request_type == "visits" or request_type == "playing":
+        uid = config.GAME_UNIVERSE_ID
+        return f'https://games.roblox.com/v1/games?universeIds={uid}'
+    elif request_type == "members":
+        gid = config.GROUP_ID
+        return f'https://groups.roblox.com/v1/groups/{gid}'
