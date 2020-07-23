deeeb973740d1eea350eab5409831259adff6a4c
m-etroo <50273080+m-etroo@users.noreply.github.com>
2020-07-23 15:37:14 -0400

    FInish getting motivation insert

--- handlers/motivation.py
+++ handlers/motivation.py
@@ -1,8 +1,31 @@
+import logging
+
+import requests, random
+from handlers import parse_api, api_urls
 import config
 
-def set_motivation():
-    if config.MOTIVATION_ENABLED = True:
+messages = config.MOTIVATIONAL_MESSAGES
+
+def set_motivation(tracker_visits):
+    if config.MOTIVATION_ENABLED == True:
         # Get data from Roblox API
-        request = requests.get(api_urls.url(request_type))
+        request = requests.get(api_urls.motivation_url())
+
+        # Check for good request
+        if request.status_code == 200:
+            logging.info(f'Roblox visits API request successful (motivation)') # Log
+            data = request.json() # Get request data
+        else:
+            logging.error(f'Roblox visits API request unsuccessful, likely an API or request issue - motivation will not be tracked')
+            return ""
+        
+        # Parse request data to get number
+        motivation_visits = parse_api.parse("motivation", data)
+
+        times = str(round(motivation_visits / tracker_visits, 2))
+
+        message = messages[random.randint(0, len(messages)-1)]
+        insert = f"\n\n**{config.MOTIVATION_INFORMAL_NAME}** has **{times}**x the number of visits you have! {message}"
+        return insert
     else:
         return ""
